a03d5c11f87a5651302018542c7bf4509e4a0c95
Marty Fitchen <mfitchen@gmail.com>
2016-08-12 13:36:56 -0700

    add working files with shorter meal descriptions pre-populated in the app

--- app/app.component.ts
+++ app/app.component.ts
@@ -26,6 +26,7 @@ import { MealListComponent } from './meal-list.component';
           (onMealSelect)="mealWasSelected($event)">
         </meal-list>
       </div>
+
     </div>
   `
 })
@@ -36,7 +37,7 @@ export class AppComponent {
     this.meals = [
       new Meal("Stir Fry", "Chicken stir fry with broccoli, carrots, bamboo shoots, and brown rice", 350, 0),
       new Meal("Pizza", "Pepperoni and cheese pizza", 550, 1),
-      new Meal("Salad", "Spinach salad with pine nuts, cherry tomatos, scallions, carrots, avocado, and lemon juice", 300, 2),
+      new Meal("Salad", "Spinach salad with pine nuts, scallions, carrots, avocado, and lemon juice", 300, 2),
     ];
   }
 
